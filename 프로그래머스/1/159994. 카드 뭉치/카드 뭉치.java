55d92fba405a26aa1f46c58cb0a3a6e82530e076
lllh <153933332+hjkim22@users.noreply.github.com>
2024-06-23 23:56:47 +0900

    [level 1] Title: 카드 뭉치, Time: 0.11 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        
+        for (String card : cards1) {
+            queue1.offer(card);
+        }
+        for (String card : cards2) {
+            queue2.offer(card);
+        }
+        
+        for (String word : goal) {
+            if (!queue1.isEmpty() && queue1.peek().equals(word)) {
+                queue1.poll();
+            } else if (!queue2.isEmpty() && queue2.peek().equals(word)) {
+                queue2.poll();
+            } else {
+                return "No";
+            }
+        }
+        
+        return "Yes";
+    }
+}
