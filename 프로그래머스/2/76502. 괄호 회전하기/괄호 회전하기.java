41adb2f00703d6ed8ca2a41c98a46d9264ccb99e
lllh <153933332+hjkim22@users.noreply.github.com>
2024-08-21 01:55:03 +0900

    [level 2] Title: 괄호 회전하기, Time: 46.81 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,39 +1,45 @@
-import java.util.Stack;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int solution(String s) {
-        int validCount = 0;
-        int n = s.length();
+        int answer = 0;
+        String newStr = s;
 
-        for (int i = 0; i < n; i++) {
-            if (isValid(s)) {
-                validCount++;
-            }
-            s = rotateString(s);
+        for (int i = 0; i < s.length(); i++) {
+            if (check(newStr)) answer++;
+            newStr = newStr.substring(1, s.length()) + newStr.charAt(0);
         }
 
-        return validCount;
+        return answer;
     }
+    static boolean check(String s) {
+        Stack<Character> st = new Stack<>();
 
-    private boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) return false;
-                char top = stack.pop();
-                if ((ch == ')' && top != '(') ||
-                    (ch == ']' && top != '[') ||
-                    (ch == '}' && top != '{')) {
-                    return false;
-                }
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '(':
+                    st.push('(');
+                    break;
+                case '{':
+                    st.push('{');
+                    break;
+                case '[':
+                    st.push('[');
+                    break;
+                case ')':
+                    if (st.isEmpty() || st.peek() != '(') return false;
+                    st.pop();
+                    break;
+                case '}':
+                    if (st.isEmpty() || st.peek() != '{') return false;
+                    st.pop();
+                    break;
+                case ']':
+                    if (st.isEmpty() || st.peek() != '[') return false;
+                    st.pop();
+                    break;
             }
         }
-        return stack.isEmpty();
-    }
-
-    private String rotateString(String s) {
-        return s.substring(1) + s.charAt(0);
+        return st.isEmpty();
     }
 }
