cc344a15d55e266381473423b465deb097cfcb41
lllh <153933332+hjkim22@users.noreply.github.com>
2024-08-26 12:26:14 +0900

    [level 2] Title: 땅따먹기, Time: 22.96 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,20 +1,16 @@
-public class Solution {
-    int solution(int[][] land) {
-        int n = land.length;
+import java.util.*;
+class Solution {  
+   public int solution(int[][] land) {
+       for(int i=1; i<land.length; i++){
+           land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+           land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
+           land[i][2] += Math.max(Math.max(land[i-1][1], land[i-1][0]), land[i-1][3]);
+           land[i][3] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][0]);
+       }
 
-        int[][] dp = new int[n][4];
+       int[] answer = land[land.length-1];
+       Arrays.sort(answer);
 
-        for (int i = 0; i < 4; i++) {
-            dp[0][i] = land[0][i];
-        }
-
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = land[i][0] + Math.max(dp[i-1][1], Math.max(dp[i-1][2], dp[i-1][3]));
-            dp[i][1] = land[i][1] + Math.max(dp[i-1][0], Math.max(dp[i-1][2], dp[i-1][3]));
-            dp[i][2] = land[i][2] + Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][3]));
-            dp[i][3] = land[i][3] + Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][2]));
-        }
-
-        return Math.max(dp[n-1][0], Math.max(dp[n-1][1], Math.max(dp[n-1][2], dp[n-1][3])));
-    }
+       return answer[answer.length-1];
+   }
 }
