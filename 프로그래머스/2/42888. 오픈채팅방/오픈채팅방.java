9e6f168dabb6d649536b1a90ae9d381f5d4362c7
lllh <153933332+hjkim22@users.noreply.github.com>
2024-08-26 18:36:36 +0900

    [level 2] Title: 오픈채팅방, Time: 157.97 ms, Memory: 167 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<String> solution(String[] record) {
+        List <String> list = new ArrayList<>();
+        Map <String, String> map = new HashMap<>();
+        for(String r : record) {
+            String[] s = r.split(" ");
+            list.add(s[1] + " "+ s[0]);
+            if(s.length == 3)
+                map.put(s[1], s[2]);
+        }
+        
+        List<String> answer = new ArrayList<>();
+        for(String l : list) {
+            String[] s = l.split(" ");
+            String status = " 나갔습니다.";
+            if(s[1].equals("Enter"))
+                status = " 들어왔습니다.";
+            else if(s[1].equals("Change")) continue;
+            
+            answer.add(map.get(s[0]) + "님이" + status);
+        }
+        return answer;
+    }
+}
