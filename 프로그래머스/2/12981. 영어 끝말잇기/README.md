a274a70248f75a57c9a05c61aaa561a4507e03bf
lllh <153933332+hjkim22@users.noreply.github.com>
2024-09-19 10:06:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.25 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.21 ms
+메모리: 89.3 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 20일 00:00:37
+2024년 09월 19일 10:06:03
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,24 +1,20 @@
-import java.util.HashSet;
-
-public class Solution {
+import java.util.*;
+class Solution {
     public int[] solution(int n, String[] words) {
-        HashSet<String> usedWords = new HashSet<>();
-        usedWords.add(words[0]);
-        char lastChar = words[0].charAt(words[0].length() - 1);
-
-        for (int i = 1; i < words.length; i++) {
-            String currentWord = words[i];
-            int person = (i % n) + 1;
-            int turn = (i / n) + 1;
-
-            if (usedWords.contains(currentWord) || currentWord.charAt(0) != lastChar) {
-                return new int[] {person, turn};
+        int[] answer = {0, 0};
+        Set<String> historySet = new HashSet<>();
+        historySet.add(words[0]);
+        char prevCharacter = words[0].charAt(words[0].length() - 1);
+        
+        for (int i=1; i<words.length; i++) {
+            historySet.add(words[i]);
+            if (!words[i].startsWith(Character.toString(prevCharacter)) || historySet.size() != i+1) {
+                answer[0] = (i%n)+1;
+                answer[1] = (i/n)+1;
+                break;
             }
-
-            usedWords.add(currentWord);
-            lastChar = currentWord.charAt(currentWord.length() - 1);
+            prevCharacter = words[i].charAt(words[i].length() - 1);
         }
-
-        return new int[] {0, 0};
+        return answer;
     }
 }
