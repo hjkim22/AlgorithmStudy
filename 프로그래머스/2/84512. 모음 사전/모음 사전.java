1f791e9ad4e58ca4575c76ff96d3e94b009466dc
lllh <153933332+hjkim22@users.noreply.github.com>
2024-11-22 00:02:27 +0900

    [level 2] Title: 모음 사전, Time: 2.46 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,25 @@
+class Solution {
+    static int answer = 0;
+    static int cnt = 1;
+    public int solution(String word) {
+        char[] vowel = {'A', 'E', 'I', 'O', 'U'};
+        StringBuilder sb = new StringBuilder();
+        dfs(word, vowel, sb);
+        return answer;
+    }
+    
+    public static void dfs(String word, char[] vowel, StringBuilder sb) {
+        if(sb.toString().length() >= 5)
+            return;
+        for(int i = 0; i < vowel.length && answer == 0; i++) {
+            sb.append(vowel[i]);
+            if(word.equals(sb.toString())) {
+                answer = cnt;
+                break;  
+            }
+            cnt++;
+            dfs(word, vowel, sb);
+            sb.deleteCharAt(sb.toString().length() - 1);
+        }
+    }
+}
