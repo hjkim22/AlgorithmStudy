2f3a2fa4d257d44c61128abfba5e9f8e23f06ef9
lllh <153933332+hjkim22@users.noreply.github.com>
2024-12-15 16:53:00 +0700

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.04 ms
+메모리: 79.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 18일 23:59:59
+2024년 12월 15일 18:52:56
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,19 +1,24 @@
-public class Solution {
-    public int solution(int N, int A, int B) {
-        int round = 0;
-
-        if (A > B) {
-            int temp = A;
-            A = B;
-            B = temp;
-        }
-
-        while (A != B) {
-            A = (A + 1) / 2;
-            B = (B + 1) / 2;
-            round++;
-        }
-
-        return round;
+class Solution{
+    public int solution(int n, int a, int b) {
+		int answer = 1;
+		int left = 0;
+		int right = 0;
+		if (a > b) {
+			right = a;
+			left = b;
+		} else {
+			left = a;
+			right = b;
+		}
+		while (n != 0) {
+			n /= 2;
+			if (left % 2 == 1 && right - left == 1) {
+				break;
+			}
+			left = (left + 1) / 2;
+			right = (right + 1) / 2;
+			answer++;
+		}
+		return answer;
     }
 }
