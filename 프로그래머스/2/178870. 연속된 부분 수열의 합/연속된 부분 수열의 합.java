b315e5e07cff3dc048c17d9d3f7bb4615f2f51d3
hjkim22 <153933332+hjkim22@users.noreply.github.com>
2025-01-18 22:47:24 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 80.05 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,35 +1,43 @@
-class Solution {
-    public int[] solution(int[] sequence, int k) {        
-
-    	int left = 0;
-    	int right = 0;
-    	int sum = 0;
-    	int size = sequence.length;
-    	int ans1 = 0;
-    	int ans2 = 0;
-    	
-    	for(right=0; right<sequence.length; right++) {    		
-    		sum += sequence[right];
-    		
-    		while(sum > k) {
-    			sum -= sequence[left];
-    			left++;
-    		}
-    		
-    		if(sum == k) {
-    			if(size > right-left) {
-    				size = right-left;
-    				ans1 = left;
-    				ans2 = right;
-    			}
-    			else if(size == right-left) {
-    				ans1 = Math.min(ans1, left);
-    				ans2 = Math.min(ans2, right);
-    			}
-    		}
-    		
-    	}
-    	
-    	return new int[] {ans1, ans2};
-    }
+class Solution {  
+  
+    public int[] solution(int[] sequence, int k) {  
+        int[] prefix = new int[sequence.length + 1];  
+        for (int i = 1; i < prefix.length; i++) {  
+            prefix[i] = prefix[i - 1] + sequence[i - 1];  
+        }  
+        
+        int[] result = new int[2];  
+        int min = Integer.MAX_VALUE;  
+        for (int i = 0; i < prefix.length; i++) {  
+            int right = findRight(prefix, i, k);  
+            if (right == prefix.length) {  
+                break;  
+            }  
+            int sum = prefix[right] - prefix[i];  
+            if (sum == k) {  
+                if (right - i < min) {  
+                    min = right - i;  
+                    result[0] = i;  
+                    result[1] = right - 1;  
+                }  
+            }  
+        }  
+  
+        return result;  
+    }  
+  
+    private int findRight(int[] prefix, int left, int k) {  
+        int lo = left;  
+        int hi = prefix.length - 1;  
+        while (lo < hi) {  
+            int mid = lo + (hi - lo) / 2;  
+            int sum = prefix[mid] - prefix[left];  
+            if (sum < k) {  
+                lo = mid + 1;  
+            } else {  
+                hi = mid;  
+            }  
+        }  
+        return lo;  
+    }  
 }
