b687b08ff56d6db93b06d3b60d167cfff588cf9e
hjkim22 <153933332+hjkim22@users.noreply.github.com>
2025-02-06 00:09:37 +0900

    [level 2] Title: 택배상자, Time: 182.39 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,25 +1,43 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int[] order) {
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        int size = order.length;
-        int index = 0;
-        for(int i = 1 ; i <= size ; i++){
-            if(order[index] != i){
-                stack.push(i);
-            }else{
-                index++;
-                answer++;
-            }
-            while(!stack.isEmpty() && stack.peek() == order[index]){
-                stack.pop();
-                index++;
-                answer++;
+        
+        int len = order.length;
+        Queue<Integer> main = new LinkedList<>();
+        for(int i = 1; i <= len; i++){
+            main.offer(i);
+        }
+        
+        Stack<Integer> sub = new Stack<>();
+       
+        int now = 0;
+        for(int o : order){
+            while(true){
+                if(o < now){
+                    if(sub.peek() == o){
+                        now = sub.pop();
+                        answer++;
+                        break;
+                    }
+                    else{
+                        return answer;
+                    }
+                }
+                else if(!main.isEmpty()){
+                    if(main.peek() == o){
+                        now = main.poll();
+                        answer++;
+                        break;
+                    }
+                    else{
+                        sub.push(main.poll());
+                    }
+                }
             }
         }
+        
         return answer;
     }
 }
