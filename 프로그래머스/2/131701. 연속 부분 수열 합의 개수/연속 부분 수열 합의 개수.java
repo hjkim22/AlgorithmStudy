a65c54f3c276f5cc35804d00483c110b8756193d
hjkim22 <153933332+hjkim22@users.noreply.github.com>
2025-03-27 23:34:55 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1596.12 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,15 +1,43 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public int solution(int[] elements) {
-        Set <Integer> set = new HashSet<>();
-        for(int i = 0; i < elements.length; i++) {
-            int sum = 0;
-            for(int j = 0; j < elements.length; j++) {
-                sum += elements[(i + j) % elements.length];
-                set.add(sum);
+        Set<Integer> sums = new HashSet<>();
+
+        int len = elements.length;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < len; j++) {
+                sums.add(find_sum(elements, j, j + i));
             }
         }
-        return set.size();
+
+        return sums.size();
+    }
+
+    int find_sum(int[] elements, int start, int end) {
+        int sum = 0;
+        int len = elements.length;
+        for (int i = start; i < end; i++) {
+            sum += elements[i % len];
+        }
+        return sum;
+    }
+}
+
+class Solution1 {
+    public int solution(int[] elements) {
+        Set<Integer> sums = new HashSet<>();
+
+        int len = elements.length;
+        for (int i = 0; i < len; i++) {
+            int sum = 0;
+            for (int j = 0; j < len; j++) {
+                sum += elements[(i+j) % len];
+                sums.add(sum);
+            }
+        }
+
+        return sums.size();
     }
 }
