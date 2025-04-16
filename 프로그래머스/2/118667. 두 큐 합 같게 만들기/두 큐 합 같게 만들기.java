b22559fc0436538e9ee5cb0f0a5f2545828f6af4
hjkim22 <153933332+hjkim22@users.noreply.github.com>
2025-04-16 23:48:14 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 93.87 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,44 +1,36 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Solution {
-    public int solution(int[] queue1, int[] queue2) {
-        int answer = 0;
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        
-        long sum = 0;
-        long q1_sum = 0;
-        for(int i=0; i<queue1.length; i++){
-            sum += queue1[i];
-            sum += queue2[i];
-            q1_sum += queue1[i];
-            q1.add(queue1[i]);
-            q2.add(queue2[i]);
-        }
-        
-        long mid = sum/2;
-        int cnt = queue1.length*3;
-        
-        while(q1_sum!=mid){
-            if(cnt==0){
-                return -1;
-            }
-            if(q1_sum > mid){
-                int num = q1.poll();
-                q1_sum -= num;
-                q2.add(num);
-                answer++;
-            }
-            else if(q1_sum < mid){
-                int num = q2.poll();
-                q1_sum += num;
-                q1.add(num);
-                answer++;
-            }
-            cnt--;
-        }
-        
-        return answer;
-    }
-
+public class Solution {
+	public static int solution(int[] q1, int[] q2) {
+		int ans = 0;
+		
+		long total=0;//두큐의합
+		long q1Sum=0;//1번큐의합
+		Queue<Integer> qq1 = new LinkedList<Integer>();
+		Queue<Integer> qq2 = new LinkedList<Integer>();
+		for(int i=0; i<q1.length; i++) { 
+			total += q1[i]+q2[i];
+			q1Sum += q1[i];
+			qq1.add(q1[i]);
+			qq2.add(q2[i]);
+		}
+		if(total%2!=0) return -1;//만약 두큐의합이 홀수면 같게 못만듦.
+		
+		long target = total/2;
+		while(true) {
+			if(ans>(q1.length+q2.length)*2) return -1;//이땐 더 순회해도 못만듦
+			if(q1Sum==target) break;
+			else if(q1Sum>target) {
+				q1Sum-=qq1.peek();
+				qq2.add(qq1.poll());
+			}else {
+				q1Sum += qq2.peek();
+				qq1.add(qq2.poll());
+			}
+			ans++;
+		}
+		
+		return ans;
+	}
 }
