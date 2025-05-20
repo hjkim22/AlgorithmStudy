a31be3bbdbb23d4a5a66b81224a1c258c10f7571
hjkim22 <153933332+hjkim22@users.noreply.github.com>
2025-05-21 02:25:50 +0900

    [level 3] Title: 야근 지수, Time: 119.55 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,33 +1,16 @@
 import java.util.*;
-
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        Arrays.sort(works);
+        PriorityQueue <Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+    	for(int w : works)
+            queue.add(w);
+        for(int i = 0; i < n; i++)
+            queue.offer(queue.poll() - 1);
         
-        int maxWork = works[works.length-1];
-        int len = maxWork+1;
-        int[] leftWork = new int[len];
-        
-        for(int tmp : works) leftWork[tmp]++;
-        
-        while(maxWork > 0 && n > 0){
-            if(leftWork[maxWork] == 0) maxWork--;
-            else if(leftWork[maxWork] <= n){
-                leftWork[maxWork-1] += leftWork[maxWork];
-                n -= leftWork[maxWork];
-                leftWork[maxWork] = 0;
-                maxWork--;
-            } else {
-                leftWork[maxWork-1] += n;
-                leftWork[maxWork] -= n;
-                n = 0;
-            }
-        }
-        
-        for(int i = 1 ; i < len ; i++)
-            answer += Math.pow(i, 2)*leftWork[i];
-        
+        for(int q : queue)
+            if(q > 0)
+                answer += q * q;
         return answer;
     }
 }
