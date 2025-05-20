acf23c66295741db6058bfd6c34eee5a6206e55f
hjkim22 <153933332+hjkim22@users.noreply.github.com>
2025-05-20 22:43:36 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,53 +1,22 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
-    static int[][] node;
-    static boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        
-        node = new int[n][n];
-        visited = new boolean[n];
-        
-        for(int i=0; i<computers.length; i++){
-            for(int j=0; j<n; j++){
-                if(computers[i][j] == 1) node[i][j] = node[j][i] = 1;
-            }
-        }
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                //dfs(n, i);
-                bfs(n, i);
-                answer ++;
-            }
+        boolean[] check = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(check[i]) continue;
+            DFS(n, computers, check, i);
+            answer++;
         }
         return answer;
     }
     
-    static void dfs(int n, int index){
-        visited[index] = true;
-        
-        for(int i=0; i<n; i++){
-            if(node[index][i] == 1 && !visited[i]) dfs(n, i);
-        }
-    }
-    
-    static void bfs(int n, int index){
-        Queue<Integer> que = new LinkedList<Integer>();
-        que.offer(index);
-        visited[index] = true;
-        
-        while(!que.isEmpty()){
-            int temp = que.poll();
-            
-            for(int i=0; i<n; i++){
-                if(node[temp][i] == 1 && !visited[i]){
-                    que.offer(i);
-                    visited[i] = true;
-                }
+    private void DFS(int n, int[][] computers, boolean[] check, int index) {
+        check[index] = true;
+        for(int i = 0; i < n; i++) {
+            if(!check[i] && computers[index][i] == 1) {
+                DFS(n, computers, check, i);
             }
         }
     }
